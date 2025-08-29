533335c9b94ba59fdc643491cd02c7d9843ff8ae
Gwyneth <eudoragranger0719@gmail.com>
2025-08-29 20:02:52 +0800

    Remove filer code in explore.js

--- scripts/explore.js
+++ scripts/explore.js
@@ -8,43 +8,30 @@ function renderFoodCardByCuisine(cuisine, containerSelector, state = "all") {
     food => food.cuisine === cuisine && (state === "all" || food.state === state)
   );
 
-  if (filterFoods.length === 0) {
-    foodsHTML = `
-      <div class="swiper-slide">
-        <div class="card no-food">
-          <div class="card-content">
-            <h3 class="card-title">No food available</h3>
-            <p class="card-text">Try selecting another state.</p>
+  filterFoods.forEach((food) => {
+    foodsHTML += `
+      <div class="card swiper-slide">
+        <div class="card-image">
+          <img src="${food.image}" alt="${food.name} image">
+          <p class="card-tag">${food.state}</p>
+        </div>
+        <div class="card-content">
+          <h3 class="card-title">${food.name}</h3>
+          <h6>Location:</h6>
+          <p class="card-text">${food.location}</p>
+          <h6>Description:</h6>
+          <p class="card-text">${food.description}</p>
+          <div class="card-footer">
+            <div class="product-rating-container">
+              <img class="product-rating-stars" src="${food.getStarsUrl()}" alt="Rating: 4.5">
+              <div class="product-rating-count link-primary">${food.rating.count}</div>
+            </div>
+              <a href="https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(window.location.href)}" target="_blank" class="card-button">Share</a>
           </div>
         </div>
       </div>
     `;
-  } else {
-    filterFoods.forEach((food) => {
-      foodsHTML += `
-        <div class="card swiper-slide">
-          <div class="card-image">
-            <img src="${food.image}" alt="${food.name} image">
-            <p class="card-tag">${food.state}</p>
-          </div>
-          <div class="card-content">
-            <h3 class="card-title">${food.name}</h3>
-            <h6>Location:</h6>
-            <p class="card-text">${food.location}</p>
-            <h6>Description:</h6>
-            <p class="card-text">${food.description}</p>
-            <div class="card-footer">
-              <div class="product-rating-container">
-                <img class="product-rating-stars" src="${food.getStarsUrl()}" alt="Rating: 4.5">
-                <div class="product-rating-count link-primary">${food.rating.count}</div>
-              </div>
-                <a href="https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(window.location.href)}" target="_blank" class="card-button">Share</a>
-            </div>
-          </div>
-        </div>
-      `;
-    });
-  }
+  });
 
   document.querySelector(containerSelector).innerHTML = foodsHTML;
 }
